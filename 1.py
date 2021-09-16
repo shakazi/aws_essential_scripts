37acdf36047abdfd6dc75a5794e605be66d630e6
Sharekh <87165243+shakazi@users.noreply.github.com>
2021-09-16 20:00:50 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,27 @@
+import boto3
+import csv
+  
+def addtocsv(data):
+    file = open('claimNumbers.csv', 'a+', newline ='')
+    with file:    
+        write = csv.writer(file)
+        write.writerows(data)
+
+def list_bucket_keys(bucket_name):
+    s3_client = boto3.client("s3")
+    #result = s3_client.list_objects(Bucket=bucket_name, Prefix="April20-sep20-batch-1/policy/", Delimiter="/")
+    
+    paginator = s3_client.get_paginator('list_objects_v2')
+    page_iterator = paginator.paginate(Bucket=bucket_name, Prefix="total-data-dump/policy/", Delimiter="/")
+    res_list = []
+    for page in page_iterator:
+        res = [d['Prefix'].split('/')[-2] for d in page['CommonPrefixes']]
+        res_list.append(res)
+    return res_list
+
+if __name__ == '__main__':
+    data = list_bucket_keys("tagic-claims-data")
+    print('Data gathered')
+    addtocsv(data)
+    print('Data loaded')
+        
